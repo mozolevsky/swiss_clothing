6b5e698f37c2e3e23ecedf9926df62a6b9b8fcf6
mozolevsky <alf.rulit@gmail.com>
2017-05-31 00:29:59 +0300

    Top block, filter block done

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,1 +1,9 @@
+'use strict';
 
++(function() {
+    var burger = document.querySelector('.burger');
+
+    burger.addEventListener('click', function() {
+        this.classList.toggle("change");
+    });
+})();
